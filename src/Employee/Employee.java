dc18bc2ebc34cea60b0bfcce1de8283e40b1ab18
Wojt <w.g.michalik@gmail.com>
2017-08-21 20:36:38 +0200

    dokonczyc pracownika, zrobic testy do pracownika i prostokata i kola

---
+++ src/Employee/Employee.java
@@ -0,0 +1,7 @@
+package Employee;
+
+/**
+ * Created by RENT on 2017-08-21.
+ */
+public class Employee {
+}
---
+++ src/Rectangle/RectangleTest.java
@@ -0,0 +1,7 @@
+package Rectangle;
+
+/**
+ * Created by RENT on 2017-08-21.
+ */
+public class RectangleTest {
+}
