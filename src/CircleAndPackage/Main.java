67195f9150081101b343ed95e7c8cc44b402ca56
Wojt <w.g.michalik@gmail.com>
2017-08-22 19:33:03 +0200

    Zrobiłem testy do CircleAndCylinder

---
+++ src/CircleAndPackage/Main.java
@@ -0,0 +1,7 @@
+package CircleAndPackage;
+
+/**
+ * Created by RENT on 2017-08-22.
+ */
+public class Main {
+}
---
+++ src/Point2DAndPoint3D/Point2D.java
@@ -0,0 +1,7 @@
+package Point2DAndPoint3D;
+
+/**
+ * Created by RENT on 2017-08-22.
+ */
+public class Point2D {
+}
