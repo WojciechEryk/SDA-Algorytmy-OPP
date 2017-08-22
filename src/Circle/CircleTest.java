c7dcc8566b3fdb8562be8d27c553838a7334397d
WojciechPC\Wojciech <w.g.michalik@gmail.com>
2017-08-22 15:34:28 +0200

    Testy jednostkowe do koła

--- src/Circle/CircleTest.java
+++ src/Circle/CircleTest.java
@@ -5,10 +5,49 @@ import org.junit.Assert;
 import org.junit.Test;
 
 public class CircleTest {
-    Circle cos3 = new Circle();
 
 @Test
     public void checkGetRadius(){
-    Assert.assertEquals(1, cos3.getRadius());
+    Circle cos3 = new Circle();
+    Assert.assertEquals(1, cos3.getRadius(),0.1);
 }
+    @Test
+    public void checkGetRadiusFromConstruction(){
+        Circle cosZWymiarem = new Circle(3.0);
+        Assert.assertEquals(3, cosZWymiarem.getRadius(),0.1);
+    }
+
+    @Test
+    public void checkSetRadiusfordefaultConstrucition(){
+        Circle cos3 = new Circle();
+        cos3.setRadius(2);
+        Assert.assertEquals(2, cos3.getRadius(),0.1);
+    }
+    @Test
+    public void checkSetRadiusForNotDefaultConstruction(){
+        Circle cosZWymiarem = new Circle(3.0);
+        cosZWymiarem.setRadius(2);
+        Assert.assertEquals(2, cosZWymiarem.getRadius(),0.1);
+    }
+    @Test
+    public void checkGetAreaForDefaultConstruction(){
+        Circle cos3 = new Circle();
+        Assert.assertEquals(3.14, cos3.getArea(),0.1);
+    }
+    @Test
+    public void checkGetAreaForNotDefaultConstruction(){
+        Circle cosZWymiarem = new Circle(3.0);
+        Assert.assertEquals(28.27, cosZWymiarem.getArea(),0.1);
+    }
+    @Test
+    public void checkGetCircumFerenceForDefaultCircle(){
+        Circle cos3 = new Circle();
+        Assert.assertEquals(6.28, cos3.getCircumference(),0.1);
+    }
+    @Test
+    public void checkGetCircumFerenceForNotDefaultCircle(){
+        Circle cosZWymiarem = new Circle(3.0);
+        Assert.assertEquals(18.85, cosZWymiarem.getCircumference(),0.1);
+    }
+
 }
