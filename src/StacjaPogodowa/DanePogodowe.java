1e0ac758d6112603746714aa94258efec675114c
Wojt <w.g.michalik@gmail.com>
2017-08-29 18:56:20 +0200

    Dalsze zmiany w pogodzie.

--- src/StacjaPogodowa/DanePogodowe.java
+++ src/StacjaPogodowa/DanePogodowe.java
@@ -1,16 +1,44 @@
 package StacjaPogodowa;
 
 
-public class DanePogodowe {
+import ZabawyZInterfejsem.IPojazd;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class DanePogodowe implements IPodmiot {
 
     //deklaracja zmiennych obiektowych
+    private List<IObserwator> obserwatorzy;
+    private float temp;
+    private float cisnienie;
+    private float wilgotnosc;
+
+    public DanePogodowe(){
+        obserwatorzy = new ArrayList<>(100);
+    }
+
     public void odczytyZmiennych() {
         float temp = pobierzTemperature();
         float wilgotnosc = pobierzWilgotnosc();
         float cisnienie = pobierzCisnienie();
 
-        warunkiBiezacaWyswietl.aktualizacja(temp, wilgotnosc, cisnienie);
-        statystykaWyswietl.aktualizacja(temp, wilgotnosc, cisnienie);
-        prognozaWyswietl.aktualizacja(temp, wilgotnosc, cisnienie);
+    }
+
+    @Override
+    public void zarejestrujObserwatora(IObserwator o) {
+        obserwatorzy.add(o);
+    }
+
+    @Override
+    public void usunObserwatora(IObserwator o) {
+        obserwatorzy.remove(o);
+    }
+
+    @Override
+    public void powiadomObserwatorow() {
+        for (IObserwator obserwator: obserwatorzy){
+            obserwator.aktualizacja(temp,wilgotnosc,cisnienie);
+        }
     }
 }
--- src/StacjaPogodowa/IPodmiot.java
+++ src/StacjaPogodowa/IPodmiot.java
@@ -1,7 +1,7 @@
 package StacjaPogodowa;
 
 public interface IPodmiot {
-    void zarejestrujObserwatora(Object o);
-    void usunObserwatora(Object o);
+    void zarejestrujObserwatora(IObserwator o);
+    void usunObserwatora(IObserwator o);
     void powiadomObserwatorow();
 }
