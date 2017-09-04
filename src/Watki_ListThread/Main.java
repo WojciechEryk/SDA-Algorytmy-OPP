9801614e2cbc41945faccb32d166c8312251ac23
Wojt <w.g.michalik@gmail.com>
2017-09-04 20:20:56 +0200

    Caly czas watki.

---
+++ src/Watki_ListThread/Main.java
@@ -0,0 +1,7 @@
+package Watki_ListThread;
+
+/**
+ * Created by RENT on 2017-09-04.
+ */
+public class Main {
+}
---
+++ src/Watki_ListThread/Watki_ListThread.java
@@ -0,0 +1,7 @@
+package PACKAGE_NAME;
+
+/**
+ * Created by RENT on 2017-09-04.
+ */
+public class Watki_ListThread {
+}
